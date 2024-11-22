6f7994caba06350545ff70b74694d82329032eda
Manohar_Mittapally <mittapallymanohar194@gmail.com>
2024-11-22 13:05:38 +0530

    Added the java code logic

--- src/main/java/org/consistent/practice/dsa/day1/RateLimiter.java
+++ src/main/java/org/consistent/practice/dsa/day1/RateLimiter.java
@@ -16,18 +16,28 @@ public class RateLimiter {
 
     public boolean rateLimit(int customerId){
         //Get the currentWindowNumber based on the windowSize
+        long currentWindowNumber = System.currentTimeMillis()/windowSize*1000;
 
         //Read the customer from the map using customer ID
         //if customer doesn't exist, create new customer with request count 0 and currentWindowNumber
+        Customer customer = customers.computeIfAbsent(customerId,(x)->new Customer(0,currentWindowNumber));
 
         //if currentWindowNumber > customer window number
         //reset the customer window number, since the window number past requested by customer is in past window
         //the values should be reset to current window number and request count to zero
+        if(currentWindowNumber > customer.getWindowNumber()){
+            customer.setRequestCount(0);
+            customer.setWindowNumber(currentWindowNumber);
+        }
 
         //if customer request count < request limit
         // increment the customer request count + 1 and return false to indicate that the request should not be rate limited.
+        if(customer.getRequestCount() < requestLimit){
+            customer.setRequestCount(customer.getRequestCount()+1);
+            return false;
+        }
 
         //else rate limit the request by sending true
-        return false;
+        return true;
     }
 }
